30cf15bccff532b03b494aff062cc3117a59b668
AlexeyMikhylov <alexeymikhylov02@gmail.com>
2024-03-11 23:18:47 +0300

    Add project files.

---
+++ Source.c
@@ -0,0 +1,247 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include <stdlib.h>
+
+#define SIZE 10
+
+int main()
+{
+    run();
+	return 0;
+}
+
+int run(void)
+{
+    int number;
+    printf("\nInput number: \n");
+    if (scanf("%d", &number) != NULL)
+
+    switch (number)
+    {
+    case 0:
+        puts("Exit");
+        exit(0);
+    case 1:
+        example1(); run();
+    case 2:
+        example2(); run();
+    case 3:
+        example3(); run();
+    case 41:
+        example4_1(); run();
+    case 42:
+        example4_2(); run();
+    case 5:
+        example5(); run();
+    case 6:
+        example6(); run();
+    case 7:
+        example7(); run();
+    case 8:
+        example8(); run();
+    default:
+        break;
+    }
+}
+
+void swap(void* a, void* b, size_t size);
+
+int example1(void)
+{
+    float a = 300.456;
+    float b = 0.645;
+
+    printf("a = %.3f\nb = %.3f\n", a, b);
+
+    swap(&a, &b, sizeof(float));
+
+    printf("\n");
+    printf("a = %.3f\nb = %.3f\n\n", a, b);
+}
+
+// функция меняет местами две переменные
+// memcpy(куда, откуда, сколько памяти);
+void swap(void* a, void* b, size_t size)
+{
+    void* tmp = malloc(size);
+
+    memcpy(tmp, a, size);
+    memcpy(a, b, size);
+    memcpy(b, tmp, size);
+
+    free(tmp);
+}
+
+//удаляем элемент из массива по индексу и сдвигаем остаток влево
+int example2(void)
+{
+    int a[SIZE] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    unsigned index;
+    int i;
+
+    for (i = 0; i < SIZE; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    printf("Enter index [0 .. 9]\n");
+    scanf("%ud", &index);
+
+    if (index < SIZE)
+    {
+        index = index;
+    }
+    else
+    {
+        index = SIZE - 1;
+    }
+
+    memcpy(&a[index], &a[index + 1], sizeof(int) * (SIZE - index - 1));
+    
+    for (i = 0; i < SIZE; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n\n");
+}
+
+//копируем участок строки и вставляем его 
+int example3(void)
+{
+    char str[] = "memmove can be very useful......";
+    puts(str);
+    memmove(str + 20, str + 15, 11); //'very'
+    puts(str);
+}
+
+//копирование одной строки в другую c помощью strcpy(куда, откуда)
+int example4_1(void)
+{
+    char buffer[128];
+    char* word = NULL;
+
+    scanf("%127s", buffer);
+
+    word = (char*)malloc(strlen(buffer) + 1);
+
+    strcpy(word, buffer);
+
+    printf("%s\n", word);
+
+    free(word);
+}
+
+example4_2(void)
+{
+    char buffer[128];
+    char* word = NULL;
+    char* other = NULL;
+    
+    scanf("%127s", buffer);
+    
+    word = (char*)malloc(strlen(buffer) + 1);
+    
+    other = strcpy(word, buffer);
+    
+    printf("%s\n", other);
+
+    free(other);
+}
+
+//strncpy(куда, откуда, сколько)
+//при копировании из строкив эту же строку, не должно быть пересечения
+int example5(void)
+{
+    char word[] = "Aloha, Hawaii";
+    char aloha[20];
+    char hawaii[20];
+
+    printf("%s", word);
+
+    strncpy(aloha, word, 5); //5 первых букв
+    aloha[5] = 0; //конец строки
+
+    strncpy(hawaii, &word[7], 7); //7 первых начиная с элемента (буквы) с индексом 7
+    hawaii[7] = 0; //конец строки
+
+    printf("\n%s\n%s\n", aloha, hawaii);
+    printf("\n%s, %s\n", aloha, hawaii);
+}
+
+//конкатенация (склеивание) строк
+//strcat(1ая строка, 2ая строка) -> приклеивает 2ую строку к концу 1ой, возвращает измененную 1ую.
+//первый символ 2ой строки ставится на места нулевого (\0) первой
+//
+//strncat(1ая, 2ая, сколько символов)
+int example6(void)
+{
+    char a[80];
+    char b[80];
+
+    scanf("%s", a);
+    scanf("%s", b);
+    
+    strncat(a, b, strlen(b) / 2); //приклеивает половину второй строчки
+    
+    printf("\n%s\n", a);
+}
+
+//сравнение строк
+//strcmp(1ая, 2ая) -> 0 - строки равны; 1 = 1ая больше; -1 = вторая больше;
+//qsort() - функция сортировки (из <stdlib.h>)
+
+int cmp(const void* a, const void* b);
+
+//сортировка строк по первым трем символам
+int cmp(const void* a, const void* b)
+{
+    return strncmp((char*)a, (char*)b, 3);
+}
+
+int example7(void)
+{
+    int i;
+    char words[5][79] =
+    {
+        "Solar",
+        "Obscurus",
+        "Tempestus",
+        "Ultima",
+        "Pacificus"
+    };
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("%s\n", words[i]);
+    }
+
+    qsort(words, 5, 80, cmp); //words - массив, 5 - строк, 80 - размер строк, cmp - возвращаемое значение функции cmp
+
+    printf("\n");
+    for (i = 0; i < 5; i++)
+    {
+        printf("%s\n", words[i]);
+    }
+}
+
+//поиск
+//memchr()
+int example8(void)
+{
+    char str[] = "Hello World!";
+    char* ptr = NULL;
+
+    printf("%s\n", str);
+
+    ptr = (char*)memchr(str, '\0', 4000);
+
+    if (ptr != NULL)
+    {
+        printf("first zero byte address is %p, strlen = %d", ptr, ptr - str);
+    }
+    else
+    {
+        printf("no null byte in memory block");
+    }
+}
